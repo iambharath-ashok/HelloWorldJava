f10be9a9ece4e0852df4ca309b81595a67748c5e
BharathAshok <iambharath.ashoka@gmail.com>
2017-06-04 22:23:10 +0530

    Jekins_Demo

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,10 +3,8 @@ public class HelloWorld{
 	public static void main(String[] args){
 	
 		System.out.println("Start");
-	
 		for(int i =0;i<10; i++)
 		System.out.println("Hello World : "+ i);
-		
 		System.out.println("End");
 	}
 
