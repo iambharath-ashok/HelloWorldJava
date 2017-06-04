21c39d5f2f9d044fa6ec3ea5269e4863dff6b60d
BharathAshok <iambharath.ashoka@gmail.com>
2017-06-04 20:40:28 +0530

    Meassage

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld{
 
 	public static void main(String[] args){
-		System.out.println("Hello World");
+	
+		for(int i =0;i<10; i++)
+		System.out.println("Hello World : "+ i);
 	}
 
 }
